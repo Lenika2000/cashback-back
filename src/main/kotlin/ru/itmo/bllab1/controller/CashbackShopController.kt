e27db01dd8eb7f8034a0313ca714355d5c6605ae
Elena Manshina <Lenika2000@yandex.ru>
2021-02-26 20:17:30 +0300

    Вынесение логики по редактированию кэшбека в отдельный сервис

--- src/main/kotlin/ru/itmo/bllab1/controller/CashbackShopController.kt
+++ src/main/kotlin/ru/itmo/bllab1/controller/CashbackShopController.kt
@@ -1,10 +1,9 @@
 package ru.itmo.bllab1.controller
 
 import org.springframework.web.bind.annotation.*
-import ru.itmo.bllab1.repo.Cashback
-import ru.itmo.bllab1.repo.CashbackRepository
-import ru.itmo.bllab1.repo.CashbackStatus
-import ru.itmo.bllab1.repo.ShopRepository
+import ru.itmo.bllab1.repo.*
+import ru.itmo.bllab1.service.CashbackService
+import ru.itmo.bllab1.service.MessageService
 import javax.persistence.EntityNotFoundException
 
 data class CashbackChangeRequestPayload(
@@ -18,32 +17,22 @@ data class CashbackChangeRequestPayload(
 @RequestMapping("/api/shop/cashback/")
 @RestController
 class CashbackShopController(
+        private val cashbackService: CashbackService,
         private val cashbackRepository: CashbackRepository,
-        private val shopRepository: ShopRepository
+        private val shopRepository: ShopRepository,
 ) {
 
     @PostMapping("update")
     fun updateCashback(@RequestBody payload: CashbackChangeRequestPayload): CashbackResponse {
-        val cashback = cashbackRepository.findById(payload.id).orElseThrow {
-            EntityNotFoundException("Кэшбек с id ${payload.id} не найден!")
-        }
-        payload.isPaid?.let { cashback.isPaid = it }
-        payload.isOrderCompleted?.let { cashback.isOrderCompleted = it }
-        payload.payment?.let {
-            cashback.confirmPayment = true
-            cashback.cashbackSum = it
-        }
-        if (cashback.status == CashbackStatus.NEW)
-            cashback.status = CashbackStatus.RECEIVED_INF
-        cashbackRepository.save(cashback)
-        return CashbackResponse("Изменение информации о кэшбэке принято к обработке", cashback.id)
+        val cashbackId = cashbackService.updateCashback(payload)
+        return CashbackResponse("Изменение информации о кэшбэке принято к обработке", cashbackId)
     }
 
     @GetMapping("getAll/{shopId}")
-    fun getClientCashbacks(@PathVariable shopId: Long): List<Cashback> {
+    fun getShopCashbacks(@PathVariable shopId: Long): List<Cashback> {
         val shop = shopRepository.findById(shopId).orElseThrow {
             EntityNotFoundException("Магазин с id $shopId не найден!")
         }
-        return cashbackRepository.findCashbackByShop(shop);
+        return cashbackRepository.findCashbackByShop(shop)
     }
 }
--- src/main/kotlin/ru/itmo/bllab1/service/CashbackService.kt
+++ src/main/kotlin/ru/itmo/bllab1/service/CashbackService.kt
@@ -2,10 +2,12 @@ package ru.itmo.bllab1.service
 
 import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Service
+import ru.itmo.bllab1.controller.CashbackChangeRequestPayload
 import ru.itmo.bllab1.repo.CashbackRepository
 import ru.itmo.bllab1.repo.CashbackStatus
 import ru.itmo.bllab1.repo.ClientRepository
 import java.time.LocalDateTime
+import javax.persistence.EntityNotFoundException
 
 @Service
 class CashbackService(
@@ -18,14 +20,30 @@ class CashbackService(
         cashbackRepository.findByStartDateLessThan(LocalDateTime.now().minusDays(60)).forEach { cashback ->
             if (cashback.status != CashbackStatus.NEW && cashback.isPaid && cashback.isOrderCompleted && cashback.confirmPayment) {
                 cashback.status = CashbackStatus.APPROVED
-                cashback.client.balance += cashback.cashbackSum * 0.9;
-                clientRepository.save(cashback.client);
+                cashback.client.balance += cashback.cashbackSum * 0.9
+                clientRepository.save(cashback.client)
                 messageService.sendNotificationToClient("Кэшбек зачислен", cashback.client )
             } else {
-                cashback.status = CashbackStatus.REJECTED;
+                cashback.status = CashbackStatus.REJECTED
                 messageService.sendNotificationToClient("Кэшбек отклонен", cashback.client )
             }
-            cashbackRepository.save(cashback);
+            cashbackRepository.save(cashback)
         }
+    }
+
+    fun updateCashback(payload: CashbackChangeRequestPayload): Long {
+        val cashback = cashbackRepository.findById(payload.id).orElseThrow {
+            EntityNotFoundException("Кэшбек с id ${payload.id} не найден!")
+        }
+        payload.isPaid?.let { cashback.isPaid = it }
+        payload.isOrderCompleted?.let { cashback.isOrderCompleted = it }
+        payload.payment?.let {
+            cashback.confirmPayment = true
+            cashback.cashbackSum = it
+        }
+        if (cashback.status == CashbackStatus.NEW)
+            cashback.status = CashbackStatus.RECEIVED_INF
+        cashbackRepository.save(cashback)
+        return cashback.id
     }
 }

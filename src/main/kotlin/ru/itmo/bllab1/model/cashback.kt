a5ad736dec109c9eb2d307db7603ba8f50a3aa7b
Elena Manshina <Lenika2000@yandex.ru>
2021-02-26 20:20:48 +0300

    Вынесение сущностей в пакет model

---
+++ src/main/kotlin/ru/itmo/bllab1/model/cashback.kt
@@ -0,0 +1,30 @@
+import java.time.LocalDateTime
+import javax.persistence.*
+
+enum class CashbackStatus {
+    NEW,
+    RECEIVED_INF,
+    APPROVED,
+    REJECTED,
+}
+
+@Entity
+class Cashback(
+        @Id
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+        var id: Long = 0,
+        var startDate: LocalDateTime = LocalDateTime.now(),
+        @ManyToOne
+        var client: Client = Client(),
+        @ManyToOne
+        var shop: Shop = Shop(),
+        @Column(name = "is_paid")
+        var isPaid: Boolean = false,
+        @Column(name = "is_order_completed")
+        var isOrderCompleted: Boolean = false,
+        @Column(name = "confirm_payment")
+        var confirmPayment: Boolean = false,
+        var status: CashbackStatus = CashbackStatus.NEW,
+        @Column(name = "cashback_sum")
+        var cashbackSum: Double = 0.0
+)
--- src/main/kotlin/ru/itmo/bllab1/repo/ShopRepository.kt
+++ src/main/kotlin/ru/itmo/bllab1/repo/ShopRepository.kt
@@ -1,16 +1,9 @@
 package ru.itmo.bllab1.repo
 
+import Shop
 import org.springframework.data.repository.CrudRepository
 import java.util.*
 import javax.persistence.*
-
-@Entity
-class Shop(
-        @Id
-        @GeneratedValue(strategy = GenerationType.IDENTITY)
-        var id: Long = 0,
-        var name: String = "",
-)
 
 interface ShopRepository : CrudRepository<Shop, Long> {
         fun findShopByName(name: String): Optional<Shop>
